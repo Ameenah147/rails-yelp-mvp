0ea5248d566ffabfc52d964f8bc1d97f47ef1d98
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-05 14:39:19 +0400

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+epicure = {name: "Epicure", address: "75008, Paris", phone_number:"1234567", category:"french"}
+roti = {name: "Roti", address: "chemin parla, pres lamem", phone_number:"1234567", category:"italian"}
+burger = {name: "Burger", address: "750, Belgique", phone_number:"1234567", category:"belgian"}
+
+
+[epicure, roti, burger].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
